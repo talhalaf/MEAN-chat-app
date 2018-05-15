f8e9d351e3035dbcf08b25d6c27a24483bbb4a80
Tal Halaf <talhalaf@outlook.com>
2018-05-15 16:20:56 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect',function(){
     console.log('Connected to server');
-
-    socket.emit('createMessage',{
-        to:'jen@example.com',
-        text: 'Hi, this is Tal!',
-        createdAt: new Date()
-    });
 });
 
 socket.on('disconnect',function(){
