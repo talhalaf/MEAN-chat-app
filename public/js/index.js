c29b95474372909df3bdcdeb4b722ba7de6a47bb
Tal Halaf <talhalaf@outlook.com>
2018-05-18 19:19:07 +0300

    Added auto-scrolling for new messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,22 @@ socket.on('disconnect',function(){
     console.log('Disconnected from server');
 });
 
+function scrollToBottom(){
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('newMessage',function(message){
     var template = jQuery('#message-template').html();
     var html = Mustache.render(template,{
@@ -17,7 +33,7 @@ socket.on('newMessage',function(message){
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
